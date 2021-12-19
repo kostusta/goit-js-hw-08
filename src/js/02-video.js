02bf9476905b015e63418651a2627c584af96a8a
Stanislav Kostiuchenko <kostiusta@gmail.com>
2021-12-19 22:28:07 +0200

    Adds done task#2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,14 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on('timeupdate', throttle(videoplayerCurrentTime, 1000));
+
+function videoplayerCurrentTime(timeupdate) {
+  localStorage.setItem('videoplayer-current-time', timeupdate.seconds);
+  return timeupdate.seconds;
+}
+
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));

aaa2548cd582119f79e039c6e8ad5794fb441551
Stanislav Kostiuchenko <kostiusta@gmail.com>
2021-12-21 23:38:57 +0200

    Adds done task #3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,26 +1,39 @@
-import throttle from "lodash.throttle"
+import throttle from 'lodash.throttle';
 
 const refs = {
   feedbackForm: document.querySelector('.feedback-form'),
+  emailFeedbackForm: document.querySelector('input[type="email"]'),
+  messageFeedbackForm: document.querySelector('textarea[name="message"]'),
   submitButton: document.querySelector('.feedback-form button[type="submit"]'),
 };
 
+const STORAGE_KEY = 'feedback-form-state';
+
 function onFeedbackFormInput(event) {
-  const [input, textarea] = event.currentTarget.elements;
   const feedbackFormState = {
-    email: input.value,
-    message: textarea.value,
+    email: refs.emailFeedbackForm.value,
+    message: refs.messageFeedbackForm.value,
   };
-  localStorage.setItem('feedback-form-state', JSON.stringify(feedbackFormState));
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(feedbackFormState));
 }
 
 function onFeedbackFormSubmit(event) {
   event.preventDefault();
 
-  console.log(JSON.parse(localStorage.getItem('feedback-form-state')));
-  localStorage.removeItem('feedback-form-state');
+  console.log(JSON.parse(localStorage.getItem(STORAGE_KEY)));
+  localStorage.removeItem(STORAGE_KEY);
   event.currentTarget.reset();
+}
+
+function onPageLoadHandler() {
+  const feedbackFormState = JSON.parse(localStorage.getItem(STORAGE_KEY));
+  if (feedbackFormState) {
+    const { email, message } = refs.feedbackForm.elements;
+    email.value = feedbackFormState.email;
+    message.value = feedbackFormState.message;
+  }
 }
 
 refs.feedbackForm.addEventListener('input', throttle(onFeedbackFormInput, 500));
 refs.feedbackForm.addEventListener('submit', onFeedbackFormSubmit);
+window.addEventListener('load', onPageLoadHandler);
